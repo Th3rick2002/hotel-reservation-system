3837232023143fb0aa22b66e2d7831e002377519
aristides <fuenteslfns@gmail.com>
2024-10-07 19:20:07 -0600

    Agregando CRUD de Usuarios

---
+++ DataAccessLayer/DbConnection/SqlDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DataAccessLayer.DbConnection
+{
+    public class SqlDataAccess
+    {
+        private readonly string _connectionString;
+
+        public SqlDataAccess()
+        {
+            _connectionString = "Data Source=LAPTOP-PHCFNULN\\SQLEXPRESS;Initial Catalog=Reservamiento_HotTel;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/UsuariosRepositories.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.DbConnection;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public  class UsuariosRepositories
+    {
+        private SqlDataAccess _dbConnection;
+
+        public UsuariosRepositories()
+        {
+            _dbConnection = new SqlDataAccess();
+        }
+
+        private DataTable GetAllUsuarios()
+        {
+            DataTable usuariosTable = new DataTable();
+
+            using (var connection = _dbConnection.GetConnection())
+            {
+                string query = "SELECT Nombre, Apellido, Email, Telefono, FechaRegistro FROM Usuario";
+                
+                using(var sqlcommand = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    using(SqlDataReader reader = sqlcommand.ExecuteReader())
+                    {
+                        usuariosTable.Load(reader);
+                    }
+                }
+            }
+
+            return usuariosTable;
+        }
+    }
+}
